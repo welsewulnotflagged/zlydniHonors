e563414375b4db63ea18af0875f20d8a4acb7b2c
teheidoma <novakovskii2002@gmail.com>
2023-09-07 12:19:03 +0300

    door

---
+++ Assets/Scripts/Interaction/Punchable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+public class Punchable : MonoBehaviour, Interactable {
+    public float PunchStrength = 500f;
+
+    private Rigidbody _rigidbody;
+
+
+    public void Start() {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void Interact(GameObject source) {
+        _rigidbody.AddForce(source.transform.TransformDirection(Vector3.forward) * PunchStrength);
+    }
+}
